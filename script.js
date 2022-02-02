8f02b4afe06e80ceef583ed42df6fb76fb706f5a
Harshana Chathuranga <91200650+chboyka@users.noreply.github.com>
2022-02-02 22:02:20 +0530

    push1

---
+++ script.js
@@ -0,0 +1,72 @@
+var box = document.getElementById('box');
+var circle = document.getElementById('circle');
+var move = 0;
+var moveCircle = 0;
+
+// var element = box
+
+
+function moveRight(element){
+    if( parseInt(getComputedStyle(box).marginLeft)+110 <=  parseInt(getComputedStyle(circle).marginLeft)){
+        var mleft = parseInt(getComputedStyle(element).marginLeft);
+        element.style.marginLeft = mleft + 10 + "px";
+    }
+    else{
+        clearInterval(move);
+        clearInterval(moveCircle);
+        move = 0;
+        moveCircle = 0
+    }
+}
+function moveLeft(element){
+    if( parseInt(getComputedStyle(box ).marginLeft)+110 <=  parseInt(getComputedStyle(circle).marginLeft)){
+        var mleft = parseInt(getComputedStyle(element ).marginLeft);
+        element.style.marginLeft = mleft - 10 + "px";
+    }
+    else{
+        clearInterval(move);
+        clearInterval(moveCircle);
+        move = 0;
+        moveCircle = 0
+    }
+}
+function moveUp(){
+    var mtop = parseInt(getComputedStyle(box).marginTop);
+    box.style.marginTop = mtop - 10 + "px";
+}
+function moveDown(){
+    var mtop = parseInt(getComputedStyle(box).marginTop);
+    box.style.marginTop = mtop + 10 + "px";
+}
+
+
+function startAnimation(){
+    if(move == 0)
+        move = setInterval(boxMove, 100);
+}
+function stopAnimation(){
+    clearInterval(move);
+    move = 0;
+}
+
+
+function keyHandler(event){
+    switch(event.key){
+        case "ArrowUp":
+            moveUp();
+            break;
+        case "ArrowDown":
+            moveDown();
+            break;
+        case "ArrowLeft":
+            moveLeft();
+            break;
+        case "ArrowRight":
+            moveRight();
+            break;
+        case " ":
+            move = setInterval(moveLeft, 100, circle);
+            moveCircle = setInterval(moveRight,100,box);    
+            break;
+    }
+}
